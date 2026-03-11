aca2fbdc4aff12b0133e98bfbc8ad35b35859297
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2026-03-11 23:47:43 +0530

    Half email authentication done

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,6 +1,12 @@
 import { registerSchema } from '../config/zod.js'
 import TryCatch from '../middlewares/TryCatch.middleware.js'
 import senitize from 'mongo-sanitize'
+import redisClient from '../config/redis.js'
+import {User} from "../models/user.model.js"
+import crypto from "crypto"
+import sendMail from '../services/send.mail.js'
+import bcrypt from 'bcrypt'
+import { getVerifyEmailHtml } from '../config/html.js'
 
 export const  registerUser =  TryCatch(async (req,res)=>{
     const senitizedBody = senitize(req.body)
@@ -29,9 +35,42 @@ export const  registerUser =  TryCatch(async (req,res)=>{
 
     const {name,email,password} = validation.data
 
-    res.status(201).json({
+    const rateLimitKey = `register-rate-limit:${req.ip}:${email}`
+
+    if(await redisClient.get(rateLimitKey)){
+        return  res.status(429).json({
+            message:"Too many request try again later",
+        })
+    }
+
+    let user = await User.findOne({email})
+
+    if(user){
+        return res.status(400).json({
+            message:"User already exists"
+        })
+    }
+
+    const hashPassword = await bcrypt.hash(password,10)
+
+    const verifyToken = crypto.randomBytes(32).toString("hex")
+    const verifyKey = `verify:${verifyToken}`
+    const dataToStore = JSON.stringify({
         name,
         email,
-        password
+        password:hashPassword
+    }) 
+
+    await redisClient.set(verifyKey,dataToStore,{EX:300})
+
+    const subject = "Verify your email Account for Register";
+    const html = getVerifyEmailHtml({email,token : verifyToken})
+
+    await sendMail({email,subject,html})
+
+    await redisClient.set(rateLimitKey,"true",{EX:60})
+
+    res.status(200).json({
+        message:"If your email is valid, averification link has been sent. It will be expired in 5 minutes."
     })
 })
--- Backend/src/services/send.mail.js
+++ Backend/src/services/send.mail.js
@@ -5,13 +5,13 @@ const sendMail = async({email,subject,html})=>{
         host:"smtp.gmail.com",
         port:465,
         auth:{
-            user:"dsdasfd",
-            pass:"casassdasd"
+            user:process.env.SMTP_USER,
+            pass:process.env.SMTP_PASSWORD
         }
     })
 
     await transport.sendMail({
-        from:"xyz@gmail.com",
+        from:process.env.SMTP_USER,
         to:email,
         subject,
         html
