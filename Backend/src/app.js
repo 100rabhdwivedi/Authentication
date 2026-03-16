b511362c5782c77de4320f1add548552ab4efb03
100rabhdwivedi <dwivedi100rabhh@gmail.com>
2026-03-16 22:59:29 +0530

    cors setup done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import userRoutes from './routes/user.route.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE","OPTIONS"]
+}))
 //using routes
 app.use('/api/v1',userRoutes)
 
